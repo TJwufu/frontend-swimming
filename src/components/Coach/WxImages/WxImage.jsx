52c3fbc5888ad86f61d681e1013ef58da835a6be
Daguod <zengguoq@foxmail.com>
2018-04-28 16:02:21 +0800

    no message

--- src/components/Coach/WxImages/WxImage.jsx
+++ src/components/Coach/WxImages/WxImage.jsx
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { Modal, List, Button, WhiteSpace, WingBlank } from 'antd-mobile';
+import { Modal, List, Button, WhiteSpace, WingBlank, Toast } from 'antd-mobile';
 import Viewer from 'react-viewer';
 import 'react-viewer/dist/index.css';
 import initReactFastclick from 'react-fastclick';
@@ -14,29 +14,39 @@ class WxImage extends Component {
 
   handleChange(e) {
     e.stopPropagation();
-    let viewerMask = document.getElementsByClassName('react-viewer-mask')[0]
-    viewerMask.style.background = '#333'
-    document.getElementById('root').style.overflow = 'hidden'
-    document.body.style.paddingRight = '0';
-    viewerMask.style.height = '100%'
-    let addClass = document.getElementsByClassName('react-viewer-close')[0]
-    let addIconClass = document.getElementsByClassName('react-viewer-icon')[0]
-    addClass.style.width = '.6rem'
-    addClass.style.height = '.6rem'
-    addClass.style.top = '.5rem'
-    addClass.style.right = '.5rem'
-    addClass.style.background = 'none'
-    addIconClass.style.fontSize = '.35rem'
+    Toast.loading('正在加载...', 1.5, () => {
+      this.setState({ visible: true }, () => {
+        document.getElementsByClassName('react-viewer-mask')[0].style.background = '#333'
+        let addClass = document.getElementsByClassName('react-viewer-close')[0]
+        document.querySelectorAll('.react-viewer-icon').forEach(div => {
+          div.style.fontSize = '.35rem'
+        })
+        document.querySelectorAll('.react-viewer-btn').forEach(div => {
+          div.style.width = '.6rem'
+          div.style.height = '.6rem'
+          div.style.background = 'none'
+        })
+        document.getElementsByClassName('react-viewer-footer')[0].style.bottom = '5%'
+        document.getElementsByClassName('react-viewer-toolbar')[0].style.height = '0.6rem'
+        document.getElementById('root').style.overflow = 'hidden'
+        document.body.style.paddingRight = '0';
+        addClass.style.width = '.6rem'
+        addClass.style.height = '.6rem'
+        addClass.style.top = '.5rem'
+        addClass.style.right = '.5rem'
+        addClass.style.background = 'none'
+      })
+    });
+
   }
   render() {
     return (
       <span>
-        <a src="#" style={{ color: 'blue' }} onClick={() => { this.setState({ visible: !this.state.visible }); }}>查看</a>
+        <a href="javascript:void(0);" style={{ color: 'blue' }} onClick={this.handleChange.bind(this)}>查看</a>
         <Viewer
           visible={this.state.visible}
           onClose={() => { this.setState({ visible: false }); document.getElementById('root').style.overflow = 'auto' }}
-          onMaskClick={this.handleChange.bind(this)}
-          images={[{ src: this.props.dataSrc, alt: '' }]}
+          images={[{ src: this.props.dataSrc }]}
           rotatable={false}
           scalable={false}
           attribute={false}
