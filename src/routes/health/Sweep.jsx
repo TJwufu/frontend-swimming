ebe5cd872484f97f9e2cb17bccdc5c715952cca2
wufu <wufu@192.168.2.158>
2018-05-31 11:03:45 +0800

    c

--- src/routes/health/Sweep.jsx
+++ src/routes/health/Sweep.jsx
@@ -12,6 +12,17 @@ class Sweep extends React.Component {
   goApply = () => {
     hashHistory.push('/healthApply')
   }
+  showScanQRCode = () => {
+    window.wx.scanQRCode({
+		needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+		scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+		success: function (res) {
+		  var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+		  console.info(result);
+          hashHistory.push('/healthCard?'+ result);
+		}
+	});	
+  }
   render() {
     return (
       <div className={styles.container}>
@@ -27,7 +38,7 @@ class Sweep extends React.Component {
         <div className={styles.dis_flx}>
           <div>
             <img src="//oiu4ewuqq.qnssl.com/swimSweep.png" alt="扫码入场" />
-            <Button type="primary" className={styles.btn} onClick={this.goApply}>去申请</Button>
+            <Button type="primary" className={styles.btn} onClick={this.showScanQRCode}>开始扫描</Button>
           </div>
         </div>
       </div>
