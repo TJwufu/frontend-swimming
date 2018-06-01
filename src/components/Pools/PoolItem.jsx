158c036bc6dd424a9ee152659f142c1bc839ef41
wufu <wufu@192.168.2.109>
2018-06-01 17:36:09 +0800

    场所管理员-视频实时查看支持。

--- src/components/Pools/PoolItem.jsx
+++ src/components/Pools/PoolItem.jsx
@@ -1,11 +1,30 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'dva/router';
+import { hashHistory } from 'dva/router';
 import StarIcons from '../Common/StarIcons';
 import styles from './PoolItem.less';
 
+let isAdminToken = (sessionStorage.getItem('adminToken') != null);
+
+// 跳转视频直播页面
+function showLiveVideo(_liveVideoUrl){
+  if(_liveVideoUrl == null || _liveVideoUrl == ''){
+  	return;
+  }
+	document.location = _liveVideoUrl;
+}
+
+//跳转游泳场所详情页面
+function toLinkPage(_id){
+	if(isAdminToken){
+		return;
+	}
+	hashHistory.push('/pools/'+_id)
+}
+
 const PoolItem = ({ rowData, sectionId, rowId }) => (
   <div className={styles.item} key={rowId}>
-    <Link to={`/pools/${rowData.id}`}>
+    <Link onClick={ toLinkPage.bind(this, rowData.id) }>
       <div className={styles.image}>
         <img style={{ height: '1.2rem', width: '100%' }} src={rowData.spAvatar !== '' ? rowData.spAvatar : 'http://img.release.1yd.me/Fnq3JmmOan-yAHtJHk-n9-o3Qqbr'} alt={rowData.spAvatar} />
       </div>
@@ -27,13 +46,25 @@ const PoolItem = ({ rowData, sectionId, rowId }) => (
           <div className={styles.sixthRow}><span>场内人数：</span><span className={styles.sixthSpan1}>暂无数据</span></div>
         </div>
         */}
-        <div className={styles.content}>
-          {rowData.idleStatus === 1 && (<div className={styles.seventhRow}><span className={styles.free}>空闲</span></div>)}
-          {rowData.idleStatus === 2 && (<div className={styles.seventhRow}><span className={styles.tofullstrength}>接近满员</span></div>)}
-          {rowData.idleStatus === 3 && (<div className={styles.seventhRow}><span className={styles.fullstrength}>满员</span> </div>)}
-          <div className={styles.thirdRow}><StarIcons currentStarNumber={rowData.score} maxStarNumber="5" /></div>
-          <div className={styles.fifthRow}>{rowData.distance}</div>
-        </div>
+        {
+          !isAdminToken?
+        	(
+        			<div className={styles.content}>
+        	          {rowData.idleStatus === 1 && (<div className={styles.seventhRow}><span className={styles.free}>空闲</span></div>)}
+        	          {rowData.idleStatus === 2 && (<div className={styles.seventhRow}><span className={styles.tofullstrength}>接近满员</span></div>)}
+        	          {rowData.idleStatus === 3 && (<div className={styles.seventhRow}><span className={styles.fullstrength}>满员</span> </div>)}
+        	          <div className={styles.thirdRow}><StarIcons currentStarNumber={rowData.score} maxStarNumber="5" /></div>
+        	          <div className={styles.fifthRow}>{rowData.distance}</div>
+        	        </div>		
+        	):
+    		(
+        			<div className={styles.content}>
+        			  <div className={styles.thirdRow}>场内人数：{rowData.currentArrivedNumber}</div>
+            		  <div className={styles.fifthRow}><img style={{ height: '0.5rem', width: '0.5rem' }} src={'http://img.release.1yd.me/FsLz8r2E01DDkDFoNvJ78zdIRlyl'}   onClick={showLiveVideo.bind(this, rowData.liveVideoUrl)}/></div>
+        	        </div>		
+        	)	
+        }
+        
       </div>
     </Link>
   </div >
