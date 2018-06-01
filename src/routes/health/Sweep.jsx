0cc731ad333a312a29bf6bc850e9ae0df03e3daa
hecy <cuike@1yd.me>
2018-06-01 11:48:39 +0800

    sweep

--- src/routes/health/Sweep.jsx
+++ src/routes/health/Sweep.jsx
@@ -15,7 +15,6 @@ class Sweep extends React.Component {
   showScanQRCode = () => {
     // 微信扫码
     wx.scanQRCode({
-      desc: "scanQRCode desc",
       needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
       scanType: ["qrCode", "barCode"], // 可以指定扫二维码还是一维码，默认二者都有
       success: function(res) {
